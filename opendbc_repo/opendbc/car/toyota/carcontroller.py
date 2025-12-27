a9a317ec2bb8573665b95d22d67be5020f4a195d
Kevin Yeh <kyeh@apple.com>
2025-12-27 14:48:58 -0800

    Indicate MADS state via LKAS_STATUS
    (cherry picked from commit 10dc8b08a383825741df33a8a84c28f0421ec73c)

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -311,7 +311,7 @@ class CarController(CarControllerBase, GasInterceptorCarController):
       if self.frame % 20 == 0 or send_ui:
         can_sends.append(toyotacan.create_ui_command(self.packer, steer_alert, pcm_cancel_cmd, hud_control.leftLaneVisible,
                                                      hud_control.rightLaneVisible, hud_control.leftLaneDepart,
-                                                     hud_control.rightLaneDepart, CC.latActive, CS.lkas_hud))
+                                                     hud_control.rightLaneDepart, CC.latActive, CS.lkas_hud, CC_SP.mads.enabled))
 
       if (self.frame % 100 == 0 or send_ui) and self.CP.flags & ToyotaFlags.DISABLE_RADAR.value:
         can_sends.append(toyotacan.create_fcw_command(self.packer, fcw_alert))
