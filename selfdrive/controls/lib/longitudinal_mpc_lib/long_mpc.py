2bda39fc608008a0a2ab43bcd41d734b94eb944c
Kevin Yeh <kyeh@apple.com>
2025-12-29 22:06:41 -0800

    Adjust following distances
    (cherry picked from commit 069e15776ff679ebc63f2be48b745c69fead8463)

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -74,9 +74,9 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45
+    return 1.3
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.25
+    return 1.1
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
